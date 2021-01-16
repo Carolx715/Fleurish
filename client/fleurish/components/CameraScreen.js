7650821a18658d1c2fa51fef41d3c1f1f20a0bb0
Jessica Zheng <jessica3710@hotmail.com>
2021-01-16 18:33:32 -0500

    Add camera feature -wip for camera permissions

--- client/fleurish/components/CameraScreen.js
+++ client/fleurish/components/CameraScreen.js
@@ -1,12 +1,66 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions'
+class CameraScreen extends React.Component {
 
-const CameraScreen = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Camera</Text>
-    </View>
-  );
+    camera = null;
+    _focusListener = null;
+    _blurListener = null;
+
+    state = {
+        hasCameraPermission: null,
+        isFocused: true,
+    };
+
+
+    async componentDidMount() {
+        const camera = await Permissions.getAsync(Permissions.CAMERA);
+        const hasCameraPermission = (camera.status === 'granted');
+
+        this._focusListener = this.props.navigation.addListener('focus', () => {
+            this.setState({ isFocused: true });
+        });
+
+        this._blurListener = this.props.navigation.addListener('blur', () => {
+            this.setState({ isFocused: false });
+        });
+
+        this.setState({ hasCameraPermission });
+    };
+
+    componentWillUnmount() {
+        if (this._focusListener) {
+            this._focusListener();
+            this._focusListener = null;
+        }
+
+        if (this._blurListener) {
+            this._blurListener();
+            this._blurListener = null;
+        }
+    }
+
+    render() {
+        const { hasCameraPermission, isFocused } = this.state;
+
+        if (hasCameraPermission === null || hasCameraPermission === false ) {
+
+        }
+
+        else if (isFocused)
+        {
+            return (
+                <View style={{ flex: 1 }}>
+                    <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+                    </Camera>
+                </View>
+            )
+        }
+
+        return null;
+    };
+
 };
 
 export default CameraScreen;
